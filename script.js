32dd0abcc8a4129052e0cf07a3383c9bb2d445b2
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-19 10:10:47 +0200

    Finish the fifth challenge

--- script.js
+++ script.js
@@ -49,3 +49,6 @@ const [gk, ...fieldPlayers] = players1;
 const allPlayers = [...players1, ...players2];
 // Question 4
 const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+// Question 5
+const {team1, x: draw, team2} = game.odds;
+console.log(draw);
