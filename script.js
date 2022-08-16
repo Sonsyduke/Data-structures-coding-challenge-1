ccdea348d602f0579f07c3a3464874e723c3af0c
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-16 10:55:52 +0200

    Question 1

--- script.js
+++ script.js
@@ -41,31 +41,43 @@ const game = {
   },
 };
 
+// ========================
+// CHALLENGE 1
+
 // Question 1:
-const [players1, players2] = game.players;
+// const [players1, players2] = game.players;
 
 // Question 2:
-const [gk, ...otherPlayers] = players1;
+// const [gk, ...otherPlayers] = players1;
 
 // Question 3:
-const allPlayers = [...players1, ...players2];
+// const allPlayers = [...players1, ...players2];
 
 // Question 4:
-const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+// const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
 
 // Question 5:
-const {team1, x: draw, team2} = game.odds;
+// const {team1, x: draw, team2} = game.odds;
 
 // Question 6:
-function printGoals(...players) {
-  for (let i = 0; i < players.length; i++) {
-    console.log(`${players[i]}`);
-  }
-}
+// function printGoals(...players) {
+//   for (let i = 0; i < players.length; i++) {
+//     console.log(`${players[i]}`);
+//   }
+// }
 
 // printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
-printGoals(...game.scored);
+// printGoals(...game.scored);
 
 // Question 7:
-team1 < team2 && console.log("Team 1 is more likely to win");
-team1 > team2 && console.log("Team 2 is more likely to win");
+// team1 < team2 && console.log("Team 1 is more likely to win");
+// team1 > team2 && console.log("Team 2 is more likely to win");
+
+// ========================
+// CHALLENGE 2
+
+// Question 1
+const goals = Object.entries(game.scored);
+for (const [num, player] of goals) {
+  console.log(`Goal ${+num + 1}: ${player}`);
+}
